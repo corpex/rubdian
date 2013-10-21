95daa1f529c254af9a8e839f22d55a3b63a5c661
Konrad Lother <konrad@corpex.de>
2013-10-22 00:39:53 +0200

    added basic history output

--- lib/rubdian/command/history.rb
+++ lib/rubdian/command/history.rb
@@ -1,17 +1,28 @@
 require "rubdian"
 require "rubdian/trollop"
+require "rubdian/database"
+require "curses"
+
 module Rubdian; module Command;
   module History
     def self.main(opts={})
       logger = Rubdian.logger
       lopts = Trollop::options do
-        opt :limit, "Limit entries", :short => "-l", :default => 50
+        opt :limit, "Limit entries", :short => "-l", :default => 0
       end
-      history = Rubdian::Command::History.filter().order(:begin).first(ltops[:limit])
-      width_max = ENV['COLUMNS']
-      printf("%s\n", "-" * width_max)
-
-      printf("%s\n", "-" * width_max)
+      if lopts[:limit] == 0
+        history = Rubdian::Database::History.filter().order(:begin)
+      else
+        history = Rubdian::Database::History.filter().order(:begin).last(lopts[:limit])
+      end
+      width_max = `tput cols`.chomp.to_i
+      line = "+#{"-" * (width_max - 2)}+"
+      puts line
+      puts line
+      history.each do |h|
+        printf("| %-20s | %-20s | %-10s | %-10s | %s |\n", h.begin, h.end, h.action, h.username, h.message)
+      end
+      puts line
 
     end
   end
