c26a43eff049e699957ff49c91117a6df2947778
Konrad Lother <konrad@corpex.de>
2013-11-08 12:23:32 +0100

    fix

--- lib/rubdian/command/collect.rb
+++ lib/rubdian/command/collect.rb
@@ -71,9 +71,6 @@ module Rubdian; module Command
         _logger.info(node.hostname) { "Using command: #{cfg['rubdian']['commands']['collect']}" }
 
         n = Rubdian::Database::Node.filter(:hostname => node.hostname).first()
-        n.updates = ""
-        n.blocks = ""
-        n.blocked = false
         if n.nil?
           n = Rubdian::Database::Node.new
           n.hostname = node.hostname
@@ -81,6 +78,10 @@ module Rubdian; module Command
             n.port = node.port
           end
         end
+        n.updates = ""
+        n.blocks = ""
+        n.blocked = false
+        n.save
         _blocks = []
         _updates = []
         executor.on_data do |data, type|
